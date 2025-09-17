7d43c5812bbe4cd4f7e7ece3b7e801f74ae0f410
Murillo Leoni <murilloleoni1999@gmail.com>
2025-09-17 14:25:47 -0300

    CRUD implemented

--- src/main/java/com/mleoni/demo_park_api/controller/UserController.java
+++ src/main/java/com/mleoni/demo_park_api/controller/UserController.java
@@ -5,10 +5,9 @@ import com.mleoni.demo_park_api.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -21,5 +20,29 @@ public class UserController {
     public ResponseEntity<User> create(@RequestBody User user) {
         user = userService.save(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getById(@PathVariable Long id) {
+        User user = userService.searchById(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<User> updatePassword(@PathVariable Long id, @RequestBody User user) {
+        user = userService.editPassword(id, user.getPassword());
+        return ResponseEntity.ok(user);
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<User>> getAll() {
+        List<User> users = userService.searchAll();
+        return ResponseEntity.ok(users);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/mleoni/demo_park_api/services/UserService.java
+++ src/main/java/com/mleoni/demo_park_api/services/UserService.java
@@ -4,8 +4,12 @@ package com.mleoni.demo_park_api.services;
 import com.mleoni.demo_park_api.entities.User;
 import com.mleoni.demo_park_api.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -17,4 +21,33 @@ public class UserService {
     public User save(User user) {
         return userRepository.save(user);
     }
+
+    @Transactional(readOnly = true)
+    public User searchById(Long id) {
+        return userRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("User not found.")
+        );
+    }
+
+    @Transactional
+    public User editPassword(Long id, String password) {
+        User user = searchById(id);
+        user.setPassword(password);
+        return user;
+    }
+
+    @Transactional(readOnly = true)
+    public List<User> searchAll() {
+        return userRepository.findAll();
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        try {
+            userRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new RuntimeException("ERROR!");
+        }
+    }
+
 }
