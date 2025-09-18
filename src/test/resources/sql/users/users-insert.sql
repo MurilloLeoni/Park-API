07bfc1f6146490a039aab978e6252fda0f45f9cd
Murillo Leoni <murilloleoni1999@gmail.com>
2025-09-18 11:51:13 -0300

    Setting up the test environment

---
+++ src/test/resources/sql/users/users-insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_users(id, username, password, role) VALUES(100, 'ana@email.com', '123456', 'ROLE_ADMIN');
+INSERT INTO tb_users(id, username, password, role) VALUES(101, 'bia@email.com', '123456', 'ROLE_CLIENT');
+INSERT INTO tb_users(id, username, password, role) VALUES(102, 'bob@email.com', '123456', 'ROLE_CLIENT');
