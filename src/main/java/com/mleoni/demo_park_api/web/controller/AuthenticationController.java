a2861ce6a4d2fff7c9f65f595a9c7c44e4d462cf
Murillo Leoni <murilloleoni1999@gmail.com>
2025-09-19 19:22:44 -0300

    testing authentication system

--- src/main/java/com/mleoni/demo_park_api/web/controller/AuthenticationController.java
+++ src/main/java/com/mleoni/demo_park_api/web/controller/AuthenticationController.java
@@ -3,7 +3,13 @@ package com.mleoni.demo_park_api.web.controller;
 import com.mleoni.demo_park_api.jwt.JwtToken;
 import com.mleoni.demo_park_api.jwt.JwtUserDetailsService;
 import com.mleoni.demo_park_api.web.controller.dto.UserLoginDTO;
+import com.mleoni.demo_park_api.web.controller.dto.UserResponseDTO;
 import com.mleoni.demo_park_api.web.controller.exception.ErrorMessage;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +24,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Authenticate", description = "Resource to proceed with authentication in the API")
 @Slf4j
 @RequiredArgsConstructor
 @RestController
@@ -27,6 +34,15 @@ public class AuthenticationController {
     private final JwtUserDetailsService detailsService;
     private final AuthenticationManager authenticationManager;
 
+    @Operation(summary = "Authenticate in API", description = "Authentication feature in the API.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Authentication completed successfully and return of a berer token .",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = UserResponseDTO.class))),
+                    @ApiResponse(responseCode = "400", description = "Invalid credentials",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "422", description = "Invalid field(s)",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
+            })
     @PostMapping("/auth")
     public ResponseEntity<?> authenticate(@RequestBody @Valid UserLoginDTO dto, HttpServletRequest request) {
         log.info("Processo de auenticação pelo login {}", dto.getUsername());
@@ -40,7 +56,7 @@ public class AuthenticationController {
             log.warn("Bad Credentials from username '{}'", dto.getUsername());
         }
         return ResponseEntity
-                .badRequest()
-                .body(new ErrorMessage(request, HttpStatus.BAD_REQUEST, "Creednciais inválidas"));
+                .unprocessableEntity()
+                .body(new ErrorMessage(request, HttpStatus.UNPROCESSABLE_ENTITY, "Credenciais inválidas"));
     }
 }
