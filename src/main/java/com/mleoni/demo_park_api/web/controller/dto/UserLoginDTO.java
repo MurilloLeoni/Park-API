80fe40f878e2563f51f011b08e67291b021461f3
Murillo Leoni <murilloleoni1999@gmail.com>
2025-09-18 21:38:27 -0300

    Authenticate controller

---
+++ src/main/java/com/mleoni/demo_park_api/web/controller/dto/UserLoginDTO.java
@@ -0,0 +1,21 @@
+package com.mleoni.demo_park_api.web.controller.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class UserLoginDTO {
+
+    @NotBlank
+    @Size(min = 6, max = 6)
+    private String username;
+
+    @NotBlank
+    @Size(min = 6, max = 6)
+    private String password;
+}
